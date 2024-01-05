41d64a9ec203319b3421c5d1dcacfb2366cd8348
guzb04 <guzmanbeyhaut@gmail.com>
2024-01-04 23:44:28 -0300

    second commit, attempt to bugfix

--- js/background.js
+++ js/background.js
@@ -1,11 +1,20 @@
+function setScrollAnimation(arrayElements, arrayInitialValues, scrollSpeed){
+    let scrollTop = window.scrollY;
+    for (let i = 0; i<arrayElements.length; i++){
+        arrayElements[i].style.bottom = arrayInitialValues[i] - scrollTop * scrollSpeed + "px";
+    }
+};
+
 document.addEventListener("DOMContentLoaded", () => {
     let grassMiddleBg = document.querySelector(".grass-middle-bg");
     let grassMiddleContainer = document.querySelector(".grass-container-middle");
+    let grassFrontBg = document.querySelector(".grass-front-bg");
+    let grassFrontContainer = document.querySelector(".grass-container-front");
 
     document.addEventListener("scroll", () => {
-        let scrollTop = window.scrollY;
-        grassMiddleContainer.style.bottom = 100 - scrollTop * 0.05 + "px";
-        grassMiddleBg.style.bottom = -scrollTop * 0.05 + "px";
-        console.log(grassMiddleContainer)
+        let arrayMiddle = [grassMiddleBg, grassMiddleContainer];
+        let arrayFront = [grassFrontBg, grassFrontContainer];
+        setScrollAnimation(arrayMiddle, [0, 100], 0.05);
+        setScrollAnimation(arrayFront, [0, 0], 0.2);
     });
 });
