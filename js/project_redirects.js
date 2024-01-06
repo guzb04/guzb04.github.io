b56498eccf11e3af3730443ed43d5b5e74c71e16
guzb04 <guzmanbeyhaut@gmail.com>
2024-01-06 00:58:29 -0300

    portfolio project HTML done

--- js/project_redirects.js
+++ js/project_redirects.js
@@ -1,11 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
     let crypted = document.getElementById("crypted-card");
     let emercado = document.getElementById("emercado-card");
+    let portfolio = document.getElementById("portfolio-card");
 
     crypted.addEventListener("click", () => {
         window.location.href = "../projects/crypted.html";
     });
     emercado.addEventListener("click", () => {
         window.location.href = "../projects/emercado.html";
+    });
+    portfolio.addEventListener("click", ()=>{
+        window.location.href = "../projects/portfolio.html";
     });
 });
