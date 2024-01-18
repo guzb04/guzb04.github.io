2c0770551987c3cdba77a37fdede771c15db81ee
guzb04 <guzmanbeyhaut@gmail.com>
2024-01-18 17:18:10 -0300

    added CPE certificate to certificates.html

--- js/certificates_redirects.js
+++ js/certificates_redirects.js
@@ -1,11 +1,11 @@
-document.addEventListener("DOMContentLoaded", ()=>{
-    
+document.addEventListener("DOMContentLoaded", () => {
     let japCard = document.getElementById("jap-card");
+    let c2Card = document.getElementById("c2-card");
 
-    japCard.addEventListener("click", ()=>{
-        window.location.href = "./certificates/jap_certificate.pdf"
-    })
-
-
-
-})
+    japCard.addEventListener("click", () => {
+        window.location.href = "./certificates/jap_certificate.pdf";
+    });
+    c2Card.addEventListener("click", () => {
+        window.location.href = "./certificates/StatementOfResult.pdf";
+    });
+});
